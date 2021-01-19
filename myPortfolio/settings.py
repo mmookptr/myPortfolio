bfc0d91b1c1e02fb5941ba5771144eecaf5e9f56
mmookptr <patteera.lik@ku.th>
2021-01-20 03:11:37 +0700

    update setting.py

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
-STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -27,7 +26,8 @@ SECRET_KEY = '^t@t9w4mi(=^_eceelf(l=4m1&!98abvo^+y5l1x6^#ui7hetv'
 DEBUG = True
 
 ALLOWED_HOSTS = ['http://mmookpptr.pythonanywhere.com/',
-                   'mmookpptr.pythonanywhere.com', ]
+                   'mmookpptr.pythonanywhere.com',
+                   '127.0.0.1', ]
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 
 STATIC_ROOT = "/home/mmookpptr/myPortfolio/static"
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+# STATICFILES_DIRS = [
+#     STATIC_DIR,
+# ]
+STATIC_DIR = os.path.join(BASE_DIR, "static")
