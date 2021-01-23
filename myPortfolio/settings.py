1acb5d3b75764bdc540dcc16aaa91d756bd96c14
mmookptr <patteera.lik@ku.th>
2021-01-23 23:04:52 +0700

    re-arrange coponents

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -122,7 +123,6 @@ USE_TZ = True
 
 STATIC_ROOT = "/home/mmookpptr/myPortfolio/static"
 STATIC_URL = "/static/"
-# STATICFILES_DIRS = [
-#     STATIC_DIR,
-# ]
-STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
